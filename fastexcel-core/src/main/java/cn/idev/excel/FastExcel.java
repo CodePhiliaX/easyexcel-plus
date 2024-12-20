4fb2e077b8ecd664a3390629bb3bcd934c99f353
psxjoy <psxjoy@outlook.com>
2024-12-20 23:22:43 +0800

    feat:remove convertToPDF function

--- fastexcel-core/src/main/java/cn/idev/excel/FastExcel.java
+++ fastexcel-core/src/main/java/cn/idev/excel/FastExcel.java
@@ -1,29 +1,10 @@
 package cn.idev.excel;
 
-import cn.idev.excel.fileconvertor.ExcelConverter;
-import cn.idev.excel.fileconvertor.FileConverterContext;
-
-import java.io.File;
-
 /**
  * This is actually {@link FastExcelFactory}, and short names look better.
  *
  * @author jipengfei
  */
 public class FastExcel extends FastExcelFactory {
-
-    /**
-     * Convert excel to pdf
-     *
-     * @param excelFile excel file
-     * @param pdfFile   pdf file
-     * @param fontPath  font path for pdf can be null
-     * @param sheets    sheet index to convert, if null convert all sheets
-     */
-    public static void convertToPdf(File excelFile, File pdfFile, String fontPath, int[] sheets) {
-        FileConverterContext context = new FileConverterContext(excelFile, pdfFile, fontPath, sheets);
-        ExcelConverter excelConverter = context.getExcelConverter();
-        excelConverter.convertToPdf();
-    }
 
 }
