82f31cb5ff1dbeb9ee7dd491456a754bb737d100
psxjoy <psxjoy@outlook.com>
2024-12-23 21:41:04 +0800

    bugfix: Fix Potential Data Read Failures Caused by Custom Data Formats.by @zyd0131 at #2319

---
+++ fastexcel-test/src/test/java/cn/idev/excel/test/temp/issue2319/Issue2319.java
@@ -0,0 +1,13 @@
+package cn.idev.excel.test.temp.issue2319;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Issue2319 {
+    private String num1;
+    private String num2;
+}
